58f7c08c6acda592b15ada24ab0027d71b359f55
Andrew Cope <andrew@thecope.net>
2018-07-17 00:23:54 -0700

    port number

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("LYL-Web running on port " + port);
 });
